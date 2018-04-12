2e23b07dd35616c536f1560e2c409a0fb87dfb16
SeaLabEZF <sean.wulwick@gmail.com>
2018-04-12 16:16:29 -0700

    More updates from Linux

--- project2/project2/DES.py
+++ project2/project2/DES.py
@@ -15,34 +15,16 @@ class ClassDES:
         pad = 8 - (len(plainText) % 8)
         data = plainText + chr(pad)*pad
 
-        print(len(data))
-        c1 = des.encrypt(data)
-        c3 = ''
+        cipherText = ''
         for i in range(0, len(data), 8):
-            c3 += str(des.encrypt(data[i:i+8]))
-
-        print len(c1)
-        print c1
-        print len(c3)
-        print c3
-        return c3
+            cipherText += des.encrypt(data[i:i+8])
+        print len(cipherText)
+        return cipherText
 
     def decrypt(self, cipherText):
         des = DES.new(self.key, DES.MODE_ECB)
-        plaintext = des.decrypt(cipherText)
+        plaintext = ''
+        for i in range(0, len(cipherText), 8):
+            plaintext += des.decrypt(cipherText[i:i+8])
         unpad = plaintext[:-ord(plaintext[len(plaintext) - 1:])]
         return unpad
-
-des = ClassDES()
-
-key = '0123456789abcdef'
-print  key
-des.setKey(key)
-text = 'abcdefghabcdefgh'
-print text
-print len(text)
-cipher = des.encrypt(text)
-print cipher
-plain = des.decrypt(cipher)
-print plain
-print len(plain)
