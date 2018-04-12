11ba29d09607149956b6f11887ab8c9d6875cb1e
SeaLabEZF <sean.wulwick@gmail.com>
2018-04-12 16:42:06 -0700

    test.txt added

--- project2/project2/DES.py
+++ project2/project2/DES.py
@@ -28,3 +28,15 @@ class ClassDES:
             plaintext += des.decrypt(cipherText[i:i+8])
         unpad = plaintext[:-ord(plaintext[len(plaintext) - 1:])]
         return unpad
+
+plaintext = 'this is a test'
+key = '0123456789abcdef'
+ciphertext = ''
+f = open("test.txt", "w")
+
+des = ClassDES()
+
+des.setKey(key)
+ciphertext = des.encrypt(plaintext)
+print ciphertext
+f.write(ciphertext)
